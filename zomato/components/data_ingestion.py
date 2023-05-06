f58481ab3955c9dfb1e94cc12993f3708716befd
ratan kumar jha <imratankj@gamil.com>
2023-05-06 10:22:23 +0530

    data trainer complete

--- zomato/components/data_ingestion.py
+++ zomato/components/data_ingestion.py
@@ -78,11 +78,11 @@ class DataIngestion:
 
 # run data_tranasformation
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_ = data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+#if __name__ == "__main__":
+ #   obj = DataIngestion()
+  #  train_data_path,test_data_path=obj.initiate_data_ingestion()
+   # data_transformation = DataTransformation()
+    #train_arr,test_arr,_ = data_transformation.initaite_data_transformation(train_data_path,test_data_path)
 
 
 # to run this
---
+++ zomato/pipeline/training_pipeline.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from zomato.logger import logging
+from zomato.exception import CustomException
+import pandas as pd
+
+from zomato.components.data_ingestion import DataIngestion
+from zomato.components.data_transformation import DataTransformation
+from zomato.components.model_trainer import ModelTrainer
+
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
